b7efe3d7c925276d4d1f2e660f1f71d9f7da29a3
hugo [itb] alonso velasco <hugo.alonso.7e5@itb.cat>
2023-12-27 18:10:07 +0100

    main.kt: impresio de tiquet de compra al deixar de comprar bitllets

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,26 @@
 fun main() {
-    var bitllets = 0
+    var bitllets:Int
     var preuBitlletsAcomulats = 0.0
-    var comprarMesBitllets = false
+    var flag:Boolean
+    var bitlletsArray=arrayListOf<String>()
+    var num = 0
     do{
-        do {
+        flag = false
+        bitllets=1
+        num++
+        while(bitllets<3 && !flag) {
 
+            println("Esto es el intento numero $num de comprar billetes")
             menuBitllets()
             var bitlletEscollit = readInt("Introdueixi un numero siusplau.", "Introdueixi un número del 1 al 5 siusplau.", 1, 5)
-
 
             menuZona()
             var zonaEscollida = readInt("Introdueixi un numero siusplau.", "Introdueixi un número del 1 al 5 siusplau.", 1 , 3)
 
-
             var bitlletFinal = tarjetaEscollida(bitlletEscollit,zonaEscollida)
 
             println("Ha escollit la opcio: $bitlletFinal")
-            bitllets++
+
 
             var preuBitllet = calculaPreuBitllet(bitlletEscollit, zonaEscollida)
 
@@ -24,14 +28,25 @@ fun main() {
 
             println("El preu del bitllet es: $preuBitllet€")
 
-            println("Vol seguir comprant? (S/N)")
+            if (bitllets<3){
+                println("Vol seguir comprant? (S/N)")
 
-            var seguirComprant = readYesNo("Introdueixi un caracter (S/N)","Introdueix una -S o una -N", 'S', 'N')
+                var seguirComprant = readYesNo("Introdueixi un caracter (S/N)","Introdueix una -S o una -N", 'S', 'N')
 
-            println(seguirComprant)
-
-        }while(comprarMesBitllets && bitllets<3)
+                if (seguirComprant=='N') flag=true
+            }else{
+                println("ya no pueds comprar mas billetes")
+                println("tienes que pagar $preuBitlletsAcomulats")
+            }
+            bitlletsArray.add("$bitlletFinal - Preu: $preuBitllet")
+            bitllets++
+        }
         println("adeu")
-    }while(true)
 
+        println("------TIQUET------")
+        for (i in 0..bitlletsArray.size-1){
+            println(bitlletsArray[i])
+        }
+        println("------------------\n")
+    }while(true)
 }
