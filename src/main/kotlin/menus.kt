af0e00cf407340c6e17e82b4ab839376a3e80621
hugo [itb] alonso velasco <hugo.alonso.7e5@itb.cat>
2023-12-27 13:18:32 +0100

    menuZona y menuBitllets añadido con exito

---
+++ src/main/kotlin/menus.kt
@@ -0,0 +1,15 @@
+fun menuBitllets(){
+    println("Quin bitllet desitja adquirir?\n" +
+            "1 - Bitllet senzill\n" +
+            "2 - TCasual\n" +
+            "3 - TUsual\n" +
+            "4 - TFamiliar\n" +
+            "5 - TJove\n")
+}
+
+fun menuZona(){
+    println("Quina zona vol viatjar?\n" +
+            "1\n" +
+            "2\n" +
+            "3\n")
+}
